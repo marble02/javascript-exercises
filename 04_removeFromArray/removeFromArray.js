fa23aeecb2b060d103d923e11b1006e8e8dfbb50
m <mm.marble02@gmail.com>
2022-08-11 16:13:46 -0600

    Did exercise in folder 04 to fix removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(args) {
+    const a = [...arguments];
+    let arr = a[0];
 
+    for (let i = 1; i < a.length; i++) {
+        arr = arr.filter(item => item !== a[i])
+    }
+
+    // console.log(arr);
+    return arr
 };
+
+// removeFromArray([1, 2, 3, 4], 3, 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
