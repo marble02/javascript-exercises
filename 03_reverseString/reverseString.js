5d59e9b81f5692d936ad8d687a6e4f4e1132fcea
m <mm.marble02@gmail.com>
2022-08-11 15:55:59 -0600

    Did exercise in folder 03 to fix reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const initial = input.split('');
+    let res = [];
+    for (let i of initial) {
+        res.unshift(i);
+    }
+    // console.log(res.join(''));
+    return res.join('')
 };
+
+// reverseString('hello')
 
 // Do not edit below this line
 module.exports = reverseString;
