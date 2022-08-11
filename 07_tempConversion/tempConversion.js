6d438e52ca41d117e32c763448f6151cf5bb3154
m <mm.marble02@gmail.com>
2022-08-11 17:50:17 -0600

    Did exercise in folder 07 to fix temperature conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,21 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  let c = (f - 32) * 5 / 9;
+  c = Math.round(c * 10) / 10;
 
+  // console.log(c);
+  return c
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  let f = c * 9 / 5 + 32;
+  f = Math.round(f * 10) / 10
 
+  // console.log(f);
+  return f
 };
+
+// ftoc(32);
+// ctof(73.2);
 
 // Do not edit below this line
 module.exports = {
