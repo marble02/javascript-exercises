fb2948cab49103fc7734878dff6c13cf522d3d24
m <mm.marble02@gmail.com>
2022-08-11 15:44:47 -0600

    Did exercise in folder 02 to fix repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let res = "";
+    for (let i = 0; i < num; i++) {
+        res += text;
+    }
+    // console.log(res);
+    return res
 };
+
+// repeatString("hey", 3)
 
 // Do not edit below this line
 module.exports = repeatString;
