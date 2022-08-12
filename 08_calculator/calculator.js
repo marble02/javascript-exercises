fc70c1cc4e5a3f7580e1c2e5a6ca9e219dcc3736
m <mm.marble02@gmail.com>
2022-08-11 18:05:54 -0600

    Did exercise in folder 08 to fix calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let ans = 1;
+  for (let i = 0; i < arr.length; i++) {
+    ans *= arr[i];
+  }
+  
+  if (arr.length > 0) {
+    return ans
+  } else {
+    return "ERROR"
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n == 0) {
+    return 1
+  }
+
+  return factorial(n - 1) * n
 };
 
 // Do not edit below this line
