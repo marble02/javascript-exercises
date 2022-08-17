8fde09779c8ab3b4011eceda4863616f4367868b
m <mm.marble02@gmail.com>
2022-08-17 13:43:52 -0600

    Complete fibonacci function to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+    
+    if (n < 0) {
+        return "OOPS"
+    } else if (n == 0 || n == 1) {
+        return 1
+    }
 
+    let a = 1;
+    let b = 1;
+    let tmp = a;
+    for (let i = 2; i < n; i++) {
+        tmp = a + b;
+        a = b;
+        b = tmp;
+    }
+    return b
 };
 
 // Do not edit below this line
