c20f1c2417ddb497697c9142c06eb46bf10f4938
m <mm.marble02@gmail.com>
2022-08-17 13:34:36 -0600

    Completed palindromes function to pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let arr = text.split('');
+    arr = arr.filter(char => /[a-zA-Z]/.test(char));
+    // console.log(arr);
+    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+        if (arr[i].toLowerCase() != arr[arr.length - i - 1].toLowerCase()) {
+            return false
+        }
+    }
+    return true
 
 };
+
+// console.log(palindromes(' race car . !'))
 
 // Do not edit below this line
 module.exports = palindromes;
