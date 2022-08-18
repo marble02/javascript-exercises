ca9d4c6b65acfa40089d8822601a472cabf433f7
m <mm.marble02@gmail.com>
2022-08-17 19:49:32 -0600

    Complete the findTheOldest function to make the tests pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+
+    function getAge(person) {
+        let age;
+        if (person.yearOfDeath == undefined) {
+            age = 2022 - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth
+        }
+        return age
+    }
+
+    return obj.reduce((oldest, current) => {
+        if (getAge(current) > getAge(oldest)) {
+            return current
+        } else {
+            return oldest
+        }
+    }, obj[0])
 
 };
 
